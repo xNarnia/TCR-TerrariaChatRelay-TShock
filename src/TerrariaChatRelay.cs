0fd9a6df4d1b86530c0fcfc54fb03b3ce5443072
Everni <princessnarnia1@gmail.com>
2019-03-09 10:45:00 -0500

    Organize files into src folder for cleaner Git

---
+++ src/TerrariaChatRelay.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Terraria.ModLoader;
+
+namespace TerrariaChatRelay
+{
+	public class TerrariaChatRelay : Mod
+	{
+		public TerrariaChatRelay()
+		{
+
+		}
+
+        public override bool HijackGetData(ref byte messageType, ref BinaryReader reader, int playerNumber)
+        {
+            // Chat Message    [25]
+            // Chat Message v2 [107]
+
+            // Test Code
+            if (messageType == 25 || messageType == 107)
+            {
+                byte PlayerId = reader.ReadByte();
+                Color color = reader.ReadRGB();
+                string msg = reader.ReadString();
+
+                // 
+            }
+
+            return false;
+        }
+    }
+}
